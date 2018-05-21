c42097fce51e8bc7af451b907416da5b81b87c05
Tanish <tanish2k09@gmail.com>
2018-05-21 18:23:51 +0530

    Change some KSM values

--- mm/ksm.c
+++ mm/ksm.c
@@ -224,7 +224,7 @@ static unsigned long ksm_rmap_items;
 static unsigned int ksm_thread_pages_to_scan = 100;
 
 /* Milliseconds ksmd should sleep between batches */
-static unsigned int ksm_thread_sleep_millisecs = 20;
+static unsigned int ksm_thread_sleep_millisecs = 500;
 
 #ifdef CONFIG_NUMA
 /* Zeroed when merging across nodes is not allowed */
@@ -239,7 +239,7 @@ static int ksm_nr_node_ids = 1;
 #define KSM_RUN_MERGE	1
 #define KSM_RUN_UNMERGE	2
 #define KSM_RUN_OFFLINE	4
-static unsigned long ksm_run = KSM_RUN_STOP;
+static unsigned long ksm_run = KSM_RUN_MERGE;
 static void wait_while_offlining(void);
 
 static DECLARE_WAIT_QUEUE_HEAD(ksm_thread_wait);
@@ -1772,14 +1772,14 @@ static void ksm_tuning_pressure(void)
 		int one_half_cpus = num_possible_cpus() >> 1;
 
 		if (num_cpus >= three_quater_cpus) {
-			ksm_thread_sleep_millisecs = 20;
-			ksm_thread_pages_to_scan = 100;
+			ksm_thread_sleep_millisecs = 50;
+			ksm_thread_pages_to_scan = 500;
 		} else if (num_cpus >= one_half_cpus) {
 			ksm_thread_sleep_millisecs = 3000;
-			ksm_thread_pages_to_scan = 200;
+			ksm_thread_pages_to_scan = 500;
 		} else {
-			ksm_thread_sleep_millisecs = 10000;
-			ksm_thread_pages_to_scan = 200;
+			ksm_thread_sleep_millisecs = 5000;
+			ksm_thread_pages_to_scan = 500;
 		}
 	}
 #endif
@@ -2328,8 +2328,6 @@ static ssize_t sleep_millisecs_store(struct kobject *kobj,
 	err = strict_strtoul(buf, 10, &msecs);
 	if (err || msecs > UINT_MAX)
 		return -EINVAL;
-
-	ksm_thread_sleep_millisecs = msecs;
 
 	return count;
 }
